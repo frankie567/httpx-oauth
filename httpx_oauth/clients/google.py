13685bb703901d464297a6230a94c6639d22bb7e
James Valentine <jvalentine@kohera.co.uk>
2020-12-14 13:26:20 +0000

    feat: get more google user profile info
    broaden support for google user profiles

--- httpx_oauth/clients/google.py
+++ httpx_oauth/clients/google.py
@@ -36,11 +36,11 @@ class GoogleOAuth2(BaseOAuth2[GoogleOAuth2AuthorizeParams]):
             base_scopes=BASE_SCOPES,
         )
 
-    async def get_id_email(self, token: str) -> Tuple[str, str]:
+    async def _get_profile_data(self, token: str) -> Dict[str, Any]:
         async with httpx.AsyncClient() as client:
             response = await client.get(
                 PROFILE_ENDPOINT,
-                params={"personFields": "emailAddresses"},
+                params={"personFields": "names,emailAddresses"},
                 headers={**self.request_headers, "Authorization": f"Bearer {token}"},
             )
 
@@ -49,11 +49,35 @@ class GoogleOAuth2(BaseOAuth2[GoogleOAuth2AuthorizeParams]):
 
             data = cast(Dict[str, Any], response.json())
 
-            user_id = data["resourceName"]
-            user_email = next(
-                email["value"]
-                for email in data["emailAddresses"]
-                if email["metadata"]["primary"]
-            )
+            return data
 
-            return user_id, user_email
+    async def get_profile(self, token: str) -> Dict[str, Any]:
+        data = await self._get_profile_data(token)
+
+        data_dict = {}
+        data_dict['user_id'] = data["resourceName"]
+        
+        data_dict['email'] = next(
+            email["value"]
+            for email in data["emailAddresses"]
+            if email["metadata"]["primary"]
+        )
+
+        data_dict['display_name'] = next(name['displayName'] for name in data['names'])
+        data_dict['first_name'] = next(name['givenName'] for name in data['names'])
+        data_dict['last_name'] = next (name['familyName'] for name in data['names'])
+    
+
+        return data_dict
+
+    async def get_id_email(self, token: str) -> Tuple[str, str]:
+        data = await self._get_profile_data(token)
+            
+        user_id = data["resourceName"]
+        user_email = next(
+            email["value"]
+            for email in data["emailAddresses"]
+            if email["metadata"]["primary"]
+        )
+
+        return user_id, user_email
