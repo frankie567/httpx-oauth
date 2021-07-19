b842b5fe3002dc42715f6991eda1ebad5ff97773
Diogo <somnium@riseup.net>
2021-07-19 09:48:03 +0100

    Fix missing types imports

--- httpx_oauth/clients/github.py
+++ httpx_oauth/clients/github.py
@@ -1,4 +1,4 @@
-from typing import Any, Dict, List, Tuple, cast
+from typing import Any, Dict, List, Optional, Tuple, cast
 
 import httpx
 from typing_extensions import TypedDict
--- httpx_oauth/clients/linkedin.py
+++ httpx_oauth/clients/linkedin.py
@@ -1,4 +1,4 @@
-from typing import Any, Dict, Tuple, cast
+from typing import Any, Dict, List, Optional, Tuple, cast
 
 import httpx
 
